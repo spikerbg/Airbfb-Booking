cc84de1ce91764c5e5d06d70fbe1b296b9a6c46f
spikerrbg <spikerrbg@gmail.com>
2023-12-24 12:09:38 +0200

    init upload

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        dsadsadsa
+      </div>
+      
+    </>
+  )
+}
+
+export default App
