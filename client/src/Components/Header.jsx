3580e7c49c1634728419d01af1c4f6294414ab7b
spikerrbg <spikerrbg@gmail.com>
2023-12-26 12:20:48 +0200

    update header

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
         <FaSearch />
         </button>
         </div>
-      <Link to="login" className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-sm shadow-gray-300 '>
+      <Link to={user?'/account':'/login'} className='flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-sm shadow-gray-300 '>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 </svg>
