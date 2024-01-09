0b1408eee576cc2c66d5d1e6aa20e9c3370a1397
spikerrbg <spikerrbg@gmail.com>
2024-01-09 17:52:31 +0200

    adding click on picture

---
+++ client/src/Page/LessComponent/ListingGallery.jsx
@@ -0,0 +1,9 @@
+
+
+export default function ListingGallery() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- client/src/Page/ListingPage.jsx
+++ client/src/Page/ListingPage.jsx
@@ -56,16 +56,16 @@ export default function ListingPage() {
                     <div>
                         {place.photos?.[0] && (
                             <div>
-                                <img className="aspect-square object-cover" src={'http://localhost:3000/uploads/' + place.photos[0]} alt=""></img>
+                                <img onClick={() => setShowAllPhotos(true)} className="aspect-square object-cover cursor-pointer" src={'http://localhost:3000/uploads/' + place.photos[0]} alt=""></img>
                             </div>
                         )}
                     </div>
                     <div className="grid gap-2 overflow-hidden">
                         {place.photos?.[1] && (
-                            <img src={'http://localhost:3000/uploads/' + place.photos[1]} alt=""></img>
+                            <img onClick={() => setShowAllPhotos(true)} className="cursor-pointer" src={'http://localhost:3000/uploads/' + place.photos[1]} alt=""></img>
                         )}
                         {place.photos?.[2] && (
-                            <img src={'http://localhost:3000/uploads/' + place.photos[2]} alt=""></img>
+                            <img onClick={() => setShowAllPhotos(true)} className="cursor-pointer" src={'http://localhost:3000/uploads/' + place.photos[2]} alt=""></img>
                         )}
                     </div>
                     <button onClick={() => setShowAllPhotos(true)} className="absolute buttom-8 right-0 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-300">Show more photos</button>
