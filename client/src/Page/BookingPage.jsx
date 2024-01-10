8edb343fcd458dbb90a75de9261771055ad048c6
spikerrbg <spikerrbg@gmail.com>
2024-01-10 16:56:51 +0200

    update booking page

--- client/src/Page/BookingPage.jsx
+++ client/src/Page/BookingPage.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import AccountNav from "../AccountNav";
-
+import axios from 'axios'
 
 export default function BookingPage() {
+  const [bookings, setBookings] = useState([])
+  useEffect(() => {
+    axios.get('/booking').then(res => {
+      setBookings(res.data)
+    })
+  }, [])
+  console.log(bookings)
   return (
     <div>
       <AccountNav />
-
+      <div>
+        {bookings?.length > 0 && bookings.map((booking, index) => {
+          <div>
+            {booking.checkIn} s {booking.checkOut}
+          </div>
+        })}
+      </div>
     </div>
   )
 }
