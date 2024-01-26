5f7d0a0be77aed6332d6afb37cb67c8f58113a59
spikerrbg <spikerrbg@gmail.com>
2024-01-26 19:47:42 +0200

    adding design

--- client/src/Page/SearchPage.jsx
+++ client/src/Page/SearchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect,useState } from 'react'
 import axios from 'axios'
 import { useLocation } from 'react-router-dom';
 import { format } from 'date-fns';
+import { Link } from 'react-router-dom'
 
 export default function SearchPage() {
   const location = useLocation();
@@ -31,12 +32,22 @@ export default function SearchPage() {
       <div>
       <p className='text-sm mx-6'>Stays from {range} for {noOfGuestsParam} guests</p>
       <h1 className='text-xl font-semibold mt-2 mb-6 mx-6'>Stay in {locationParam}</h1>
-      <p>Start Date: {startDateParam}</p>
-      <p>End Date: {endDateParam}</p>
+      {/* <p>Start Date: {startDateParam}</p>
+      <p>End Date: {endDateParam}</p> */}
       {filteredPlaces.map((place, index) => (
-        <div>
-          <img className='rounded-2xl object-fill' src={'http://localhost:3000/uploads/'+place.photos?.[0]} alt="photos" />
-          <h3 className='text-sm font-bold'>{place.address}</h3>
+        <div className='my-8 gap-4 gap-y-8 px-8 grid grid-cols-4 md:grid-cols-3 lg:grid-cols-4'>
+  <Link to={'/place/'+place._id} key={index}>
+            <div className='bg-gray-300 rounded-2xl flex mb-2'>
+            <img className='rounded-2xl object-fill' src={'http://localhost:3000/uploads/'+place.photos?.[0]} alt="photos" />
+
+            </div>
+            <div></div>
+            <h3 className='text-sm font-bold'>{place.address}</h3>
+            <h2 className='text-md text-gray-500'>{place.title}</h2>
+            <div>
+             <span className='font-bold'>{place.price}EUR</span> per night
+            </div>
+          </Link>
         </div>
       ))}
     </div>
