91914863f38e625580bfafe8130f966b99b23f0a
spikerrbg <spikerrbg@gmail.com>
2024-01-26 12:12:15 +0200

    adding style on calendar and header

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -15,8 +15,9 @@ export default function Header() {
   const [searchInpute, setSearchInpute] = useState("")
   const [startDate, setStartDay] = useState(new Date())
   const [endDate, setEndDay] = useState(new Date())
-  
-  const handleSelect = (ranges) =>{
+  const [noOfGuests, setNoOfGuests] = useState(1)
+
+  const handleSelect = (ranges) => {
     setStartDay(ranges.selection.startDate)
     setEndDay(ranges.selection.endDate)
   }
@@ -27,7 +28,7 @@ export default function Header() {
     key: 'selection'
   }
 
- 
+
   const navigate = useNavigate()
   const logout = async () => {
     await axios.post('/logout')
@@ -101,12 +102,32 @@ export default function Header() {
       </header>
       {searchInpute &&
         (
-          <div>
+          <div className='flex flex-col mx-auto items-center z-1'>
             <DateRangePicker ranges={[selectionRange]}
-            minDay={new Date()}
-            rangeColors={["#FD5861"]}
-            onChange={handleSelect}
+              minDay={new Date()}
+              rangeColors={["#f27a8a"]}
+              onChange={handleSelect}
             />
+            <div className='flex items-center mb-4 justify-between w-1/4'>
+              <div>
+              <h2 className='text-xl font-semibold'>Number of guests</h2>
+              </div>
+              <div className='flex g-4 items-center'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+              </svg>
+              <input type="number" 
+              value={noOfGuests}
+              min={1}
+              onChange={e => setNoOfGuests(e.target.value)}
+              className='w-12 pl-2 text-lg outline-none text-red-400' />
+              </div>
+
+            </div>
+            <div className='flex justify-between g-4 w-1/4'>
+              <button className='text-white bg-[#f27a8a] hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center'>Cancel</button>
+              <button className='text-white bg-primary hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center'>Search</button>
+              </div>
           </div>
         )}
     </div>
